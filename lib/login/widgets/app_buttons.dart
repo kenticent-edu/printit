8871fe0982f91630a716c79909b8573e61ab34ae
Ярослав <yaroslav@MacBook-Pro-Taras-2.local>
2023-01-25 13:45:28 +0200

    Add sliding widget

---
+++ lib/login/widgets/app_buttons.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppButtons {
+  static MaterialButton createNewGroupButton = MaterialButton(
+    onPressed: () {},
+    minWidth: double.infinity,
+    color: Colors.white,
+    textColor: Colors.black,
+    height: 48,
+    child: const Text('Create a new group'),
+  );
+
+  static Container firstGroupButton = Container(
+    height: 48,
+    decoration: const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFF0566), Colors.black],
+      ),
+    ),
+    child: MaterialButton(
+      onPressed: () {},
+      minWidth: double.infinity,
+      textColor: Colors.white,
+      child: const Text('Wedding photos   7/35'),
+    ),
+  );
+
+  static MaterialButton proceedButton = MaterialButton(
+    onPressed: () {},
+    minWidth: double.infinity,
+    color: const Color(0xFF007AFF),
+    textColor: Colors.white,
+    height: 48,
+    child: const Text('Proceed immediately'),
+  );
+}
